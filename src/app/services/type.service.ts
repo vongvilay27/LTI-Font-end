2aa77decff4437d30e9e589ec08ad965d9a2cdc5
Youathor Yiaveu <yprogramming96@gmail.com>
2018-07-01 14:15:58 +0700

    Modyfied home event and type service

--- src/app/services/type.service.ts
+++ src/app/services/type.service.ts
@@ -29,4 +29,81 @@ export class TypeService {
     return this.http.get(environment.SERVER_ADDRESS + '/api/client/type/attractions', option);
   }
 
+  getTypeAttractionses(type_id: string, country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/attractions/' + type_id
+      + '/' + country_code + '/' + latlng, option);
+  }
+
+  getTypeRestaurants(type_id: string, country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/restaurant/' + type_id
+      + '/' + country_code + '/' + latlng, option);
+  }
+
+  getTypeShelters(type_id: string, country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/shelter/' + type_id
+      + '/' + country_code + '/' + latlng, option);
+  }
+
+  getTypeTransportation(type_id: string, country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/transportation/' + type_id
+      + '/' + country_code + '/' + latlng, option);
+  }
+
+  getTourCompanies(country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/tourcompany/' + country_code
+      + '/' + latlng, option);
+  }
+
+  getAnothers(country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/another/' + country_code
+      + '/' + latlng, option);
+  }
+
+  getInternets(country_code: string, latlng: string) {
+    // console.log(bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10));
+    const header: Headers = new Headers();
+    header.append('lttoken',  environment.RAMDOM_KEY );
+    // header.append('lttoken',  bcrypt.hashSync(environment.ANONYMOUS_SECRET, 10) );
+    const option: RequestOptions = new RequestOptions({headers: header});
+    return this.http.get(environment.SERVER_ADDRESS
+      + '/api/client/type/internet/' + country_code
+      + '/' + latlng, option);
+  }
+
 }
